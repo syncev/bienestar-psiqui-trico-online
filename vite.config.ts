692aa4a2d7fa2b7fe5bcdfdf256dafaae6848fee
Santi <santiagonhernandez@gmail.com>
2026-01-11 18:05:36 -0300

    Ai changes to fix site not loading 404

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/bienestar-psiqui-trico-online/",
+  base: "/dra-julieta-hernandez/",
   server: {
     host: "::",
     port: 8080,
